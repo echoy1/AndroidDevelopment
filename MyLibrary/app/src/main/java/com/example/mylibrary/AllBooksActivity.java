a26b12e6c547044801a00fb2c86de45d3475cf5e
Eric <echoy1@terpmail.umd.edu>
2021-05-25 00:59:46 -0400

    Create Book's RecyclerView

--- MyLibrary/app/src/main/java/com/example/mylibrary/AllBooksActivity.java
+++ MyLibrary/app/src/main/java/com/example/mylibrary/AllBooksActivity.java
@@ -1,14 +1,33 @@
 package com.example.mylibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class AllBooksActivity extends AppCompatActivity {
+
+    private RecyclerView booksRecView;
+    private BookRecViewAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_books);
+
+        adapter = new BookRecViewAdapter(this);
+        booksRecView = findViewById(R.id.booksRecView);
+
+        booksRecView.setAdapter(adapter);
+        booksRecView.setLayoutManager(new GridLayoutManager(this, 2));
+
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(new Book(1, "1Q84", "Haruki Murakami", 1350, "https://images-na.ssl-images-amazon.com/images/I/41FdmYnaNuL._SX322_BO1,204,203,200_.jpg"
+                 ,"A work of maddening brilliance", "Long Description"));
+        adapter.setBooks(books);
     }
 }

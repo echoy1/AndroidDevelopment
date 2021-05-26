350158a2d324a0adcc3e1eead14bbf55ffdc0687
Eric <echoy1@terpmail.umd.edu>
2021-05-26 18:58:23 -0400

    Animations - Up button

--- MyLibrary/app/src/main/java/com/example/mylibrary/AllBooksActivity.java
+++ MyLibrary/app/src/main/java/com/example/mylibrary/AllBooksActivity.java
@@ -1,11 +1,13 @@
 package com.example.mylibrary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -20,6 +22,8 @@ public class AllBooksActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_books);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         adapter = new BookRecViewAdapter(this, "allBooks");
         booksRecView = findViewById(R.id.booksRecView);
 
@@ -28,5 +32,17 @@ public class AllBooksActivity extends AppCompatActivity {
 
 
         adapter.setBooks(Utils.getInstance().getAllBooks());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

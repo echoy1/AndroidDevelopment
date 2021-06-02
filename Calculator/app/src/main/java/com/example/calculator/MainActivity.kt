da808d9169e2b6a6159a71d6b039a622f997b988
Eric <echoy1@terpmail.umd.edu>
2021-06-01 23:17:05 -0400

    Negative Numbers easier operators

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -16,6 +16,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var lastNumeric = false
     private var lastDot = false
+    private var secPart = false
+    private var firstPart = false
+    private var operator = false
+    private  var canBeNegative = true
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,110 +27,126 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
-    
+
     fun onDigit(view: View) {
-//        val txtInput = findViewById<TextView>(R.id.txtInput)
-        binding.txtInput.append((view as Button).text)
-        lastNumeric = true
+        if ((!secPart && !operator) || (secPart && operator)) {
+            binding.txtInput.append((view as Button).text)
+            lastNumeric = true
+            firstPart = true
+        }
     }
 
     fun onClear(view: View) {
         binding.txtInput.setText("")
+        binding.txtInput2.setText("")
+        binding.txtInputSymbol.setText("")
         lastNumeric = false
         lastDot = false
+        secPart = false
+        firstPart = false
+        operator = false
+        canBeNegative = true
     }
 
     fun onDecimalPoint(view: View) {
-        if (binding.txtInput.text.toString().equals("") || lastNumeric && !lastDot) {
-            binding.txtInput.append(".")
-            lastNumeric = false
-            lastDot = true
+        if ((!secPart && !operator) || (secPart && operator)) {
+            if (binding.txtInput.text.toString()
+                    .equals("") || lastNumeric && !lastDot || binding.txtInput.text.toString()
+                    .substring(1).equals("")
+            ) {
+                binding.txtInput.append(".")
+                lastNumeric = false
+                lastDot = true
+                firstPart = true
+            }
         }
     }
 
     fun onEqual(view: View) {
         if (lastNumeric) {
-            var txtValue = binding.txtInput.text.toString()
-            var prefix = ""
+            var firstInput = binding.txtInput.text.toString()
+            var secInput = binding.txtInput2.text.toString()
+            var symbol = binding.txtInputSymbol.text.toString()
+
             try {
-                if (txtValue.startsWith("-")) {
-                    prefix = "-"
-                    txtValue = txtValue.substring(1)
+                if (!secPart && !operator) {
+                    binding.txtInput2.text = firstInput
+                } else {
+                    if (symbol.equals("+")) { // || symbol.equals("")
+                        binding.txtInput2.text =
+                            removeZeroAfterDot((secInput.toDouble() + firstInput.toDouble()).toString())
+                    } else if (symbol.equals("-")) {
+                        binding.txtInput2.text =
+                            removeZeroAfterDot((secInput.toDouble() - firstInput.toDouble()).toString())
+                    } else if (symbol.equals("*")) {
+                        binding.txtInput2.text =
+                            removeZeroAfterDot((secInput.toDouble() * firstInput.toDouble()).toString())
+                    } else if (symbol.equals("/")) {
+                        binding.txtInput2.text =
+                            removeZeroAfterDot((secInput.toDouble() / firstInput.toDouble()).toString())
+                    }
                 }
+                binding.txtInput.text = ""
+                binding.txtInputSymbol.text = ""
 
-                if(txtValue.contains("-")) {
-                    val splitValue = txtValue.split("-")
-                    var one = splitValue[0]
-                    var two = splitValue[1]
+                lastNumeric = false
+                lastDot = false
+                secPart = true
+                operator = false
+                firstPart = false
+                canBeNegative = true
 
-                    if (!prefix.isEmpty()) {
-                        one = prefix + one
-                    }
-
-                    binding.txtInput.text = removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
-
-                } else if(txtValue.contains("+")) {
-                    val splitValue = txtValue.split("+")
-                    var one = splitValue[0]
-                    var two = splitValue[1]
-
-                    if (!prefix.isEmpty()) {
-                        one = prefix + one
-                    }
-
-                    binding.txtInput.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
-
-                } else if(txtValue.contains("*")) {
-                    val splitValue = txtValue.split("*")
-                    var one = splitValue[0]
-                    var two = splitValue[1]
-
-                    if (!prefix.isEmpty()) {
-                        one = prefix + one
-                    }
-
-                    binding.txtInput.text = removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
-
-                } else if(txtValue.contains("/")) {
-                    val splitValue = txtValue.split("/")
-                    var one = splitValue[0]
-                    var two = splitValue[1]
-
-                    if (!prefix.isEmpty()) {
-                        one = prefix + one
-                    }
-
-                    binding.txtInput.text = removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
-
-                }
             } catch (e: ArithmeticException) {
                 e.printStackTrace()
             }
         }
     }
 
-    private fun removeZeroAfterDot(result: String) : String{
+    private fun removeZeroAfterDot(result: String): String {
         var value = result
         if (result.contains("0"))
-            value = result.substring(0,result.length - 2)
+            value = result.substring(0, result.length - 2)
         return value
     }
 
     fun onOperator(view: View) {
-        if (lastNumeric && !isOperatorAdded(binding.txtInput.text.toString())) {
-            binding.txtInput.append((view as Button).text)
+        if (((!secPart && !operator) || (secPart && operator)) && !firstPart && ((view as Button).text.toString()
+                .equals("-") && canBeNegative)
+        ) {
+            binding.txtInput.append("-")
             lastNumeric = false
-            lastDot = false
-        }
-    }
-
-    private fun isOperatorAdded(value: String) : Boolean {
-        return if (value.startsWith("-")) {
-            false
+            canBeNegative = false
+        } else if (firstPart && secPart && operator) {
+            onEqual(view)
+            binding.txtInputSymbol.append((view as Button).text)
+            operator = true
         } else {
-            value.contains("/") || value.contains("*")
-                    || value.contains("+") || value.contains("-")
+            if ((secPart && !operator) || (firstPart && !operator)) {
+
+                binding.txtInputSymbol.append((view as Button).text)
+
+                if (!secPart && firstPart) {
+                    binding.txtInput2.setText(binding.txtInput.text)
+                    binding.txtInput.setText("")
+                }
+
+                lastNumeric = false
+                lastDot = false
+                firstPart = false
+                secPart = true
+                operator = true
+                canBeNegative = true
+            }  // && !isOperatorAdded(binding.txtInput.text.toString())
         }
     }
-
 }
+
+
+/*private fun isOperatorAdded(value: String) : Boolean {
+    return if (!            false
+    } else {
+        value.contains("/") || value.contains("*")
+                || value.contains("+") || value.contains("-")
+    }
+}*/
+
